2faf65816674e811dff1c5425efe0176ff6c5c88
resqlinksolutions-bit <resqlink.solutions@gmail.com>
2026-01-03 13:48:28 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1F6FA3",
+        teal: "#1FB9A6",
+        mint: "#8AF1E1",
+        darkbg: "#050B0E",
+        lightbg: "#F5FAFA"
+      }
+    }
+  },
+  plugins: []
+};
